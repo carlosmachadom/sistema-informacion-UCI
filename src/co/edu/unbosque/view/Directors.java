75214fb5d5a74a0d29a76b3dabd1c758ede6d40c
PC <PC@DESKTOP-OAJK77B>
2024-05-01 14:33:50 -0500

    Elementos faltantes para listas UI

---
+++ src/co/edu/unbosque/view/Directors.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package co.edu.unbosque.view;
+
+import javax.swing.JPanel;
+
+/**
+ * 
+ */
+public class Directors extends JPanel {
+
+}
---
+++ src/co/edu/unbosque/view/EditTeamForm.java
@@ -0,0 +1,11 @@
+/**
+ * 
+ */
+package co.edu.unbosque.view;
+
+/**
+ * 
+ */
+public class EditTeamForm {
+
+}
